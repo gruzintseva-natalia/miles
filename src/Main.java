2ad9f988c202c934a7ca89e8f693529b6e35e4d7
Gruzintseva Natalia <gruzintseva.natalia@gmail.com>
2022-05-18 19:13:29 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 200;
+        int bonus = 20;
+
+        int miles = amount / bonus;
+        System.out.println(miles);
+    }
+}
